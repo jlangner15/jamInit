d785e23804dc2304805db1c294072b05301858af
jlangner15 <langnerj@mcmaster.ca>
2021-11-24 15:15:33 -0500

    added guard to valid musicians, i.e ones that exist. Completed test cases, testing boundaries and illegal inputs

--- src/Musician.java
+++ src/Musician.java
@@ -2,24 +2,26 @@ public class Musician {
     private String name;
     private int age;
     private String gender;
+    private boolean valid = false;
 
     public Musician(String Name, int Age, String Gender){
         if (!Name.equals("") && Age > 0 && !Gender.equals("N")) {
             name = Name;
             age = Age;
             gender = Gender;
+            valid = true;
         }
         else System.out.println("Please enter a valid musician");
     }
 
     public void changeName(String newName){
-        if (newName.equals("")) System.out.println("Please enter a valid name");
+        if (newName.equals("") && valid) System.out.println("Please enter a valid name");
         else name = newName;
     }
 
     public void changeAge(int newAge){
-        if (newAge < age) age = newAge;
-        else System.out.println("Age change must be older than previous age");
+        if (newAge > age && valid) age = newAge;
+        else System.out.println("Please enter a valid age");
     }
 
     public String getName() {return name;}
---
+++ src/testingMusician.java
@@ -0,0 +1,72 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+public class testingMusician {
+
+    @Test
+    void testMethod(){
+        assertTrue(true);
+    }
+
+    @Test
+    void testAddMusicians(){
+        Musician alice = new Musician("Alice Smith", 34, "F");
+        Musician bob = new Musician("Bob Shear", 20, "M");
+        Musician charlie = new Musician("Charlie", 25, "M");
+
+        assertTrue(alice.getName().equals("Alice Smith") && bob.getAge() == 20 && charlie.getGender().equals("M"));
+    }
+
+    @Test
+    void testAddIllegalMusicians(){
+        Musician zak = new Musician("", 1, "M");
+        Musician yusif = new Musician("yusif arman", 0, "M");
+        Musician xander = new Musician("xander thistle", 40, "N");
+
+        assertNull(zak.getName());
+        assertEquals(yusif.getAge(), 0);
+        assertNull(xander.getGender());
+
+    }
+
+    @Test
+    void testChangeAttributes(){
+        Musician alice = new Musician("Alice Smith", 34, "F");
+        Musician bob = new Musician("Bob Shear", 20, "M");
+        Musician charlie = new Musician("Charlie", 25, "M");
+
+        alice.changeAge(35);
+        bob.changeName("Billy Shear");
+
+        assertTrue(alice.getAge() == 35 && bob.getName().equals("Billy Shear"));
+
+    }
+
+    @Test
+    void testChangIllegalAttributes(){
+        Musician zak = new Musician("zal brescott", 35, "M");
+        Musician yusif = new Musician("yusif arman", 24, "M");
+        Musician xander = new Musician("xander thistle", 40, "F");
+
+        zak.changeAge(0);
+        yusif.changeName("");
+        yusif.changeAge(-5);
+
+        System.out.println(xander.getAge());
+
+        assertEquals(zak.getAge(), 35);
+        assertTrue(yusif.getName().equals("yusif arman"));
+        assertEquals(xander.getAge(), 40);
+
+    }
+
+
+
+}
